7bcfffd5250943083c9fa0266e3d3d007a44f66b
luksvrd <ljvy9b@gmail.com>
2023-07-24 15:33:20 -0500

    Add try/except error handling

--- app/routes/api.py
+++ app/routes/api.py
@@ -8,6 +8,8 @@ from flask import Blueprint, request, jsonify
 from app.models import User
 # Importing the get_db function from app.db to get the database connection
 from app.db import get_db
+# Importing sys module to print error messages to the terminal
+import sys
 
 # define the blueprint to be used in the app
 bp = Blueprint('api', __name__, url_prefix='/api')
@@ -21,19 +23,30 @@ def signup():
   # call get_db() function to get a database connection
   db = get_db()
 
-  # create a new user
-  newUser = User(
-    # data['username'] is the username value from the data object
-    username = data['username'],
-    email = data['email'],
-    password = data['password']
-  )
+# Wrapping creation logic in a try/except block, similar to a try/catch in JavaScript
+# If the try block fails, the except block will run sending a JSON error message to the front end
+  try:
+    # attempt creating a new user
+    newUser = User(
+      # data['username'] is the username value from the data object
+      username = data['username'],
+      email = data['email'],
+      password = data['password']
+    )
 
-  # save in database
-  # Use db.add() method to prep the INSET stmt, then db.commit() to officially update the db
-  db.add(newUser)
-  # commit() method saves all changes to the database
-  db.commit()
+    # Use db.add() method to prep the INSET stmt, then db.commit() to officially update the db
+    db.add(newUser)
+    # commit() method saves all changes to the database
+    db.commit()
+  except:
+    print(sys.exc_info()[0])
 
-# Using Flask func jsonify() to send back JSON notation that includes the ID of a new user
+    # insert failed, so rollback and send error to front end 
+    # rollback() method used to prevent app from crashing if the db.add() or db.commit() methods fail
+    db.rollback()
+    return jsonify(message = 'Signup failed'), 500
+  # An AssertionError thrown when custom validation fails
+  # An IntegrityError is thrown when a UNIQUE constraint is violated
+
+  # Using Flask func jsonify() to send back JSON notation that includes the ID of a new user
   return jsonify(id = newUser.id)
